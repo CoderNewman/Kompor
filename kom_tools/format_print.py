14c27eccb10dbf8d25ca6e8985a48d6bb7835e65
姜忠良 <zhongliang@kavout.com>
2017-10-16 18:42:46 +0800

    debug

--- kom_tools/format_print.py
+++ kom_tools/format_print.py
@@ -11,12 +11,12 @@ from configure.common_config import PRINT_KEY, PRINT_TYPE
 
 logger = log_util.get_logger('debug', log_level=logging.INFO)
 
-def jprint(*args, sep = '', file = std.out, end='\n'):  # @ReservedAssignment
+def jprint(*args, sep = '', end='\n'):  # @ReservedAssignment
     if PRINT_KEY is PRINT_TYPE.all or PRINT_KEY is PRINT_TYPE.command:  # @UndefinedVariable
     
         sys_time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
         print('[' + sys_time + ']',end=' ')
-        print(*args, sep = sep, file = file, end=end)
+        print(*args, sep = sep, end=end)
         
     if PRINT_KEY is PRINT_TYPE.all or PRINT_KEY is PRINT_TYPE.log:  # @UndefinedVariable
         if args:
